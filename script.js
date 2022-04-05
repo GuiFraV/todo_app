50de1b0ce716125c3db62b00844c2a5df8c4c88c
GuiFraV <guillaume.franguiadakis@gmail.com>
2022-04-05 11:01:20 +0200

    Good versionning of project

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
-// Variables of TOdo DOM
-const a = document.getElementById("Circle");
+// SELECTORS
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button")
 
-// When User click on button => get value in the input
-a.onclick = function() {
-    var inputValue = document.getElementById("CreateSentence").value;
-    console.log(inputValue);
-    // Reset input value
-    var inputValue = document.getElementById("CreateSentence").value = " ";
-}
+// EVENTS LISTENERS
 
-// When User click on button => put a new todo on list
-a.onclick = function() {
-    
-    var inputValue = document.getElementById("CreateSentence").value;
-
-    // Create an "li" node:
-    const node = document.createElement("li");
-    node.innerHTML = '<span></span>';
-    
-    // Create a text node:
-    const textnode = document.createTextNode(inputValue);
-    
-    // Append the text node to the "li" node:
-    node.appendChild(textnode);
-    
-    // Append the "li" node to the list:
-    document.getElementById("BodyToDo").appendChild(node);
-
-    // Reset input value
-    var inputValue = document.getElementById("CreateSentence").value = " ";
-        
-}
+// FUNCTIONS

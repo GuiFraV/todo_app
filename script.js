1dec3b3cdfabf192e162f4ce44edb61f0ff700cd
GuiFraV <guillaume.franguiadakis@gmail.com>
2022-04-07 16:30:23 +0200

    Filters function complete

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-const filterOption = document.querySelector(".filter-todo")
+const filterOption = document.querySelector(".filter-todo");
+const counterDisplay = document.querySelector(".counter");
+const clearButton = document.querySelector(".items-clear");
 
 // EVENTS LISTENERS
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", checkButton);
 filterOption.addEventListener("click", filterTodo);
+clearButton.addEventListener("click", clearCompleted);
 
 // FUNCTIONS
 function addTodo(event){
@@ -28,16 +31,34 @@ function addTodo(event){
     todoCheck.classList.add("todo-check");
     todoDiv.appendChild(todoCheck);
 
+    // Delete todo DIV
+    todoDiv.addEventListener("mouseenter", function(){
+        const crossTodo = document.createElement("button");
+        crossTodo.classList.add("cross");
+        todoDiv.appendChild(crossTodo);
+    });
+
+    todoDiv.addEventListener("mouseleave", function(){
+        const crossTodoNone = document.querySelector(".cross");
+        crossTodoNone.classList.remove("cross");
+    });
+
     // Add new Todo in todolist
     todoList.appendChild(todoDiv);
 
     // Reset todo input
     todoInput.value = "";
+
+    // Counter items
+    numberTodoDiv = document.querySelectorAll(".todo").length
+    counterDisplay.innerHTML = numberTodoDiv;
+ 
 }
 
 function checkButton(e) {
     const item = e.target;
     const todo = item.previousSibling;
+    const todoDiv = item.parentNode;
 
     // uncomment if you want to understand the target event =>
     // console.log(item)
@@ -45,38 +66,68 @@ function checkButton(e) {
     if(item.classList[0] === "todo-check"){
         item.classList.toggle("todo-check-on");
         todo.classList.toggle("todo-item-completed");
+        todoDiv.classList.toggle("completed");
+        todoDiv.classList.toggle("check");
+    }
+
+    // DeleteButton
+    if(item.classList[0] === "cross"){
+        // Counter Delete Item
+        numberTodoDiv = document.querySelectorAll(".todo").length -1;
+        counterDisplay.innerHTML = numberTodoDiv;
+        todoDiv.remove();
     }
 }
 
 function filterTodo(e){
-    const todoDiv = document.querySelector(".todo");
-    const todo = todoDiv.childNodes;
-    const test = e.target;
-    
-    console.log(todoDiv);
-    console.log(todo);
+    const todo = todoList.childNodes;
+    const target = e.target.getAttribute('name');
 
-    console.log(test);
+    const allFilter = document.querySelector(".select");
+    const activeFilter = document.querySelector(".active");
+    const completedFilter = document.querySelector(".collection");
 
     todo.forEach(function(todo){
-        switch(e.target.value){
+        switch(target){
             case "all":
                 todo.style.display = "flex";
+                allFilter.classList.add("all");
+                activeFilter.classList.remove("blue");
+                completedFilter.classList.remove("blue");
                 break;
             case "active":
-                if(todo.classList.contains("todo-item-completed")){
+                if(!todo.classList.contains("completed")){
                     todo.style.display = "flex";
+                    activeFilter.classList.add("blue");
+                    allFilter.classList.remove("all");
+                    completedFilter.classList.remove("blue");
                 }else {
                     todo.style.display = "none";
                 }
-                break;
-            case "Completed":
-                if(!todo.classList.contains("todo-item-completed")){
+                break; 
+            case "completed":
+                if(todo.classList.contains("completed")){
                     todo.style.display = "flex";
                 }else {
                     todo.style.display = "none";
+                    completedFilter.classList.add("blue");
+                    allFilter.classList.remove("all");
+                    activeFilter.classList.remove("blue");
                 }
                 break;
         }
     })
 }
+
+function clearCompleted(){
+
+    const numberCompletedTodoDiv = document.querySelectorAll(".check");
+
+    for(const elem of numberCompletedTodoDiv){
+        elem.remove();
+    }
+    // Counter items
+    numberTodoDiv = document.querySelectorAll(".todo").length;
+    counterDisplay.innerHTML = numberTodoDiv;
+}
+
